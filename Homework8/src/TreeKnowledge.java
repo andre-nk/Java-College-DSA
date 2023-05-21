0ad33b1dfaa4a229e451cb12ac63a3b72e406181
Andreas Notokusumo <andreasnotokusumo23@gmail.com>
2023-05-21 15:28:45 +0700

    :sparkles: Hoemwork 8 Update

--- Homework8/src/TreeKnowledge.java
+++ Homework8/src/TreeKnowledge.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+//Deklarasi BinaryTreeNode
 class BinaryTreeNode {
 
   String label;
@@ -18,10 +19,13 @@ class BinaryTreeNode {
     return "[" + label + ", " + left + ", " + right + "]";
   }
 }
-
 class TreePrinter {
-
-  public static void print(BinaryTreeNode root) {
+  /**
+   * Print a tree
+   *
+   * @param root tree root node
+   */
+  public void print(BinaryTreeNode root) {
     List<List<String>> lines = new ArrayList<>();
 
     List<BinaryTreeNode> level = new ArrayList<>();
@@ -56,8 +60,6 @@ class TreePrinter {
         }
       }
 
-      if (widest % 2 == 1) widest++;
-
       lines.add(line);
 
       List<BinaryTreeNode> tmp = level;
@@ -66,7 +68,7 @@ class TreePrinter {
       next.clear();
     }
 
-    int perpiece = widest + 6;
+    int perpiece = lines.get(lines.size() - 1).size() * (widest + 1);
     for (int i = 0; i < lines.size(); i++) {
       List<String> line = lines.get(i);
       int hpw = (int) Math.floor(perpiece / 2f) - 1;
@@ -86,18 +88,17 @@ class TreePrinter {
 
           // lines and spaces
           if (line.get(j) == null) {
-            for (int k = 0; k < perpiece - 2; k++) {
+            for (int k = 0; k < perpiece - 1; k++) {
               System.out.print(" ");
             }
           } else {
             for (int k = 0; k < hpw; k++) {
-              System.out.print(j % 2 == 0 ? " " : "─");
+              System.out.print(j % 2 == 0 ? " " : "-");
             }
             System.out.print(j % 2 == 0 ? "┌" : "┐");
             for (int k = 0; k < hpw; k++) {
-              System.out.print(j % 2 == 0 ? "─" : " ");
+              System.out.print(j % 2 == 0 ? "-" : " ");
             }
-            System.out.print(" ");
           }
         }
         System.out.println();
@@ -162,12 +163,12 @@ public class TreeKnowledge {
       System.out.println("Pikirkan sebuah benda!");
       while (true) {
         if (x.left == null && x.right == null) {
-          System.out.print("Apakah benda tersebut " + x.label + " (y/n)? ");
+          System.out.print("Apakah benda tersebut " + x.label + " (y/n)? "); // Menanyakan apakah benda yang dipikirkan benar dengan benda yang ditunjuk oleh node saat ini
           if (YorN()) {
-            System.out.println("Saya menang!");
+            System.out.println("Saya menang!"); // Menampilkan pesan jika tebakan benar
             break;
           } else {
-            System.out.println("Beri tahu jawabannya");
+            System.out.println("Beri tahu jawabannya"); // Meminta informasi mengenai benda yang dipikirkan
             String newAnswer = sc.next();
 
             System.out.println(
@@ -177,36 +178,39 @@ public class TreeKnowledge {
               x.label
             );
 
-            sc.nextLine(); // Consume the newline character
+            sc.nextLine(); // Mengonsumsi karakter newline
             String newQuestion = sc.nextLine();
 
-            BinaryTreeNode newLeft = new BinaryTreeNode(newAnswer, null, null);
-            BinaryTreeNode newRight = new BinaryTreeNode(x.label, null, null);
-            x.label = newQuestion;
-            x.left = newLeft;
-            x.right = newRight;
+            BinaryTreeNode newLeft = new BinaryTreeNode(newAnswer, null, null); // Membuat node baru untuk jawaban yang benar
+            BinaryTreeNode newRight = new BinaryTreeNode(x.label, null, null); // Membuat node baru untuk jawaban yang salah
+            x.label = newQuestion; // Mengubah pertanyaan pada node saat ini dengan pertanyaan baru
+            x.left = newLeft; // Menghubungkan node saat ini dengan node jawaban yang benar
+            x.right = newRight; // Menghubungkan node saat ini dengan node jawaban yang salah
             break;
           }
         } else {
-          System.out.print(x.label + " (y/n)? ");
+          System.out.print(x.label + " (y/n)? "); // Menanyakan pertanyaan berdasarkan label pada node saat ini
           if (YorN()) {
-            x = x.left;
+            x = x.left; // Pindah ke anak kiri jika jawaban adalah "ya"
           } else {
-            x = x.right;
+            x = x.right; // Pindah ke anak kanan jika jawaban adalah "tidak"
           }
         }
       }
 
-      System.out.print("Lanjut (y/n)? ");
+      System.out.print("Lanjut (y/n)? "); // Menanyakan apakah ingin melanjutkan permainan
       if (!YorN()) {
-        break;
+        break; // Berhenti jika jawaban adalah "tidak"
       }
+
+      print(); // Menampilkan struktur pohon setelah satu permainan selesai
     }
+
+    print(); // Menampilkan struktur pohon terakhir setelah selesai semua permainan
   }
 
   public static void main(String[] args) {
     TreeKnowledge tk = new TreeKnowledge(new Scanner(System.in));
     tk.run();
-    tk.print();
   }
 }
